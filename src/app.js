bcc759b9f6fc0dd3fbeee7de1abf1a79fa87fabb
Melvin Kisten <iammelvink@gmail.com>
2021-01-02 18:07:03 +0200

    JWT setup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import jsonwebtoken from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import config from './config';
 import routes from './routes/crmRoutes';
@@ -15,6 +16,25 @@ app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
+
+// JWT setup
+app.use((req, res, next) => {
+  // check headers
+  // check authorization in headers
+  // check for JWT at index 0 in array
+  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+    // then verify the secret word
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULLAPIs', (err, decode) => {
+      // if err clear user
+      req.user = decode;
+      next();
+    });
+  } else {
+    // Clear user data when sending request to user
+    req.user = undefined;
+    next();
+  }
+})
 
 // DB Config
 const db = `${MONGO_URI}/${MONGO_DB_NAME}`;
