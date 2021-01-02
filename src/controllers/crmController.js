2a59201a87cc55d5c7fbc326987110b3076aafd7
Melvin Kisten <iammelvink@gmail.com>
2021-01-02 16:44:58 +0200

    Create CRM boilerplate

---
+++ src/controllers/crmController.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+import {
+  ContactSchema
+} from '../models/crmModel';
+
+const Contact = mongoose.model('Contact', ContactSchema);
+
+// Create new contact
+export const addNewContact = (req, res) => {
+  let newContact = new Contact(req.body);
+
+  newContact.save((err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(contact);
+  });
+};
+
+// Read all contacts
+export const getContacts = (req, res) => {
+  Contact.find({}, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(contact);
+  });
+};
+
+// Read contact by id
+export const getContactByID = (req, res) => {
+  Contact.findById(req.params.contactId, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(contact);
+  });
+}
+
+// Update contact
+export const updateContact = (req, res) => {
+  Contact.findOneAndUpdate({
+    _id: req.params.contactId
+  }, req.body, {
+    new: true
+  }, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(contact);
+  })
+}
+
+// Delete contact
+export const deleteContact = (req, res) => {
+  Contact.remove({
+    _id: req.params.contactId
+  }, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({
+      message: 'Successfully deleted contact'
+    });
+  })
+}
---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,32 @@
+import {
+  addNewContact,
+  getContacts,
+  getContactByID,
+  updateContact,
+  deleteContact
+} from '../controllers/crmController';
+
+const routes = (app) => {
+  app.route('/api/contacts')
+    .get((req, res, next) => {
+      // middleware
+      console.log(`Request from: ${req.originalUrl}`)
+      console.log(`Request type: ${req.method}`)
+      next();
+    }, getContacts)
+
+    // POST endpoint
+    .post(addNewContact);
+
+  app.route('/api/contact/:contactId')
+    // get specific contact
+    .get(getContactByID)
+
+    // put request
+    .put(updateContact)
+
+    // delete request
+    .delete(deleteContact);
+}
+
+export default routes;
