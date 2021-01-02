f03361ccfa62692672739e7ab4cf387b82b913a7
Melvin Kisten <iammelvink@gmail.com>
2021-01-02 17:17:06 +0200

    Add handler for user registration

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -16,3 +16,23 @@ export const loginRequired = (req, res, next) => {
     });
   }
 }
+
+// Register a User
+export const register = (req, res) => {
+  // Create new user
+  const newUser = new User(req.body);
+  // Encrypt password
+  newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
+  // Save to database
+  newUser.save((err, user) => {
+    if (err) {
+      return res.status(400).send({
+        message: err
+      });
+    } else {
+      // Clear password when sending request to user
+      user.hashPassword = undefined;
+      return res.json(user);
+    }
+  })
+}
